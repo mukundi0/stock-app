ec2cf202225b542183979d905d2860429484fe51
Mukundi <mukundiwitness18@gmail.com>
2025-07-03 21:01:06 +0300

    stock-market app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cards from '../components/Cards';
+import Search from '../components/Search';
 
 const Home = () => {
+  const [searchString, setSearchString] = useState('');
+  const handleSearchStringUpdate = (searchString) => {
+    setSearchString(searchString);
+  };
+
   return (
     <>
-        Hello Page
+      <Search searchStringUpdated={handleSearchStringUpdate} />
+      <Cards searchString={searchString} />
     </>
   );
 }
+
 
 export default Home;
